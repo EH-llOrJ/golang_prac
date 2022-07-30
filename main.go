31a0d70848fd3e58bfeb5f878320db9d78ff4ea5
EH-llOrJ <jth4115@naver.com>
2022-07-30 18:16:26 +0900

    struct

--- main.go
+++ main.go
@@ -1,18 +1,38 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
 
-func lenAndUpper(name string) (lenght int, uppercase string) {
-	defer fmt.Println("I'm done")
-	lenght = len(name)
-	uppercase = strings.ToUpper(name)
-	return
+// func lenAndUpper(name string) (lenght int, uppercase string) {
+// 	defer fmt.Println("I'm done")
+// 	lenght = len(name)
+// 	uppercase = strings.ToUpper(name)
+// 	return
+// }
+
+// func main() {
+// 	totalLenght, up := lenAndUpper("taehoon")
+// 	fmt.Println(totalLenght, up)
+// }
+
+// func superAdd(numbers ...int) int {
+// 	fmt.Println(numbers)
+// 	return 1
+// }
+
+// func main() {
+// 	superAdd(1, 2, 3, 4, 5, 6)
+// }
+
+type person struct {
+	name    string
+	age     int
+	favFood []string
 }
 
 func main() {
-	totalLenght, up := lenAndUpper("taehoon")
-	fmt.Println(totalLenght, up)
+	favFood := []string{"kimchi", "ramen"}
+	taehoon := person{"taehoon", 28, favFood}
+	// taehoon := person{name: "taehoon", age: 28, favFood: favFood}
+	// 2가지 방법으로 명시 가능
+	fmt.Println(taehoon)
 }
