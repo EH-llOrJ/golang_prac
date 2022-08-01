8367686a5c69036931a81e847dadb43271e32d71
EH-llOrJ <jth4115@naver.com>
2022-08-01 23:35:19 +0900

    nothing

--- main.go
+++ main.go
@@ -2,16 +2,6 @@ package main
 
 import "fmt"
 
-type person struct {
-	name    string
-	age     int
-	favFood []string
-}
-
 func main() {
-	favFood := []string{"kimchi", "ramen"}
-	taehoon := person{"taehoon", 28, favFood}
-	// taehoon := person{name: "taehoon", age: 28, favFood: favFood}
-	// 2가지 방법으로 명시 가능
-	fmt.Println(taehoon)
+	fmt.Println("taehoon")
 }
